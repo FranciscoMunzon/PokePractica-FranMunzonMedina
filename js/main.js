ed2857758572b8cd171dbdd6da30a4844af63b4b
Fran Munzón <francisco.munzon@a.vedrunasevillasj.es>
2025-10-21 17:05:28 +0200

    Ejercicio 2 - Fetch y renderizado de los primeros 151 Pokémon

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Ejercicio 1:
+
 const nav = document.querySelector("nav");
 const botonAzul = document.getElementById("water");
 const botonRojo = document.getElementById("fire");
@@ -17,10 +19,65 @@ function cambiarColorAmarillo() {
 }
 
 function mostrarColorOriginal() {
-    nav.style.backgroundColor = "";
+    nav.style.backgroundColor = "#78C850";
 }
 
 botonAzul.addEventListener("click", cambiarColorAzul);
 botonRojo.addEventListener("click", cambiarColorRojo);
 botonAmarillo.addEventListener("click", cambiarColorAmarillo);
 botonMostrar.addEventListener("click", mostrarColorOriginal);
+
+// Ejercicio 2:
+
+const listaPokemon = document.getElementById("listaPokemon");
+
+botonMostrar.addEventListener("click", () => {
+fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  .then((response) => response.json())
+  .then((data) => {
+    const pokemons = []; // lista temporal para guardar todos los Pokémon
+
+    data.results.forEach((pokemon) => {
+      fetch(pokemon.url)
+        .then((res) => res.json())
+        .then((pokeData) => {
+          pokemons.push(pokeData); // guardamos el Pokémon
+
+          // Cuando ya tenemos los 151, los ordenamos y mostramos
+          if (pokemons.length === 151) {
+            pokemons.sort((a, b) => a.id - b.id);
+            mostrarPokemons(pokemons);
+          }
+        });
+    });
+  });
+
+function mostrarPokemons(lista) {
+  lista.forEach((pokeData) => {
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+
+    const id = pokeData.id.toString().padStart(3, "0");
+    const tipos = pokeData.types
+      .map(tipo => `<p class="tipo ${tipo.type.name}">${tipo.type.name}</p>`)
+      .join("");
+
+    div.innerHTML = `
+      <div class="pokemon-imagen">
+        <img src="${pokeData.sprites.other["official-artwork"].front_default}" alt="${pokeData.name}">
+      </div>
+      <div class="pokemon-info">
+        <div class="nombre-contenedor">
+          <p class="pokemon-id">#${id}</p>
+          <h2 class="pokemon-nombre">${pokeData.name}</h2>
+        </div>
+        <div class="pokemon-tipos">
+          ${tipos}
+        </div>
+      </div>
+    `;
+
+    listaPokemon.appendChild(div);
+  });
+}
+});
